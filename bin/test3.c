57a0836f953552238c4eb0765f949164ffe29606
minister4416 <37448962+minister4416@users.noreply.github.com>
2018-03-21 23:08:17 +0800

    Add files via upload

---
+++ bin/test3.c
@@ -0,0 +1,20 @@
+test()
+{
+File writename = new File(outputPath);
+
+			
+    //noteLine
+            out.write(outputBuffer);
+			
+/*noteLine
+/*noteLine
+*/
+/*noteLine*/
+
+            out.flush();
+            out.close();
+}//noteLine
+for(){
+	hello world£¡
+ 
+}/*noteLine*/
